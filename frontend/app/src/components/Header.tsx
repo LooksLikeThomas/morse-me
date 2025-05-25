6e2979f24d33afa9ca4187767bb348eef13a1716
Marius Walter <mariowa80@gmail.com>
2025-05-25 16:03:27 +0200

    frontend basic structure and installation

---
+++ frontend/app/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <div className="flex flex-row justify-center">
+            <div className="font-bold">
+                Morse-Me&nbsp; -
+            </div>
+            <div className="text-xl flex items-center">
+                &nbsp; Learn Morse Code, One Friend at a Time!
+            </div>
+        </div>
+    )
+}
